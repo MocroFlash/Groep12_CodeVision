f0c914be53a97e80e6c6422ab01986becf9fcec0
DMBRR00 <2064624@talnet.nl>
2023-06-08 14:48:32 +0200

    included folder aka boogy :)

--- Boogy/Header.php
+++ Boogy/Header.php
@@ -1,1 +1,11 @@
-<?php
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Content">
+    <meta name="keywords" content="Keywords">
+    <meta name="author" content="Mounir Jouhri">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Skyhigh club</title>
+
+    <link rel="stylesheet" href="css/main.css">
+</head>
--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Content">
-    <meta name="keywords" content="Keywords">
-    <meta name="author" content="Mounir Jouhri">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Skyhigh club</title>
-
-    <link rel="stylesheet" href="css/main.css">
-</head>
+<?php
+    include "Boogy/Header.php"
+?>
 <body>
 <div class="container">
     <div class="nav">
