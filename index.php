35ac71dbc022c6d3de460ba0043e79da3ddd722e
MocroFlash <118447745+MocroFlash@users.noreply.github.com>
2023-06-13 11:13:22 +0200

    Iets cools

--- index.php
+++ index.php
@@ -4,22 +4,24 @@
     <meta charset="UTF-8">
     <meta name="description" content="Content">
     <meta name="keywords" content="Keywords">
-    <meta name="author" content="Mounir Jouhri">
+    <meta name="author" content="CodeVision">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Skyhigh club</title>
 
     <link rel="stylesheet" href="css/main.css">
 </head>
+<img src="MicrosoftTeams-image.png" alt="logo">
 <body>
 <div class="container">
-    <div class="nav">
-        <a href="#" class="nav item" >Home</a>
-        <a href="#" class="nav item" >Planner</a>
-        <a href="#" class="nav item" >About</a>
-        <a href="#" class="nav item" >Contact</a>
+    <div class="navbar">
+        <a class="active" href="#">Home</a>
+        <a href="#">Planner</a>
+        <a href="#">About</a>
+        <a href="#">contact</a>
     </div>
 </div>
 <script src="js/index.js"></script>
 </body>
 </html>
+
